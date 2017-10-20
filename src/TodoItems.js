1a4ddc494d559c4e401935477f15d0f4540717bc
Naeem <Naeem>
2017-10-20 13:27:40 +0500

    changes

---
+++ src/TodoItems.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+ 
+class TodoItems extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+ 
+    this.createTasks = this.createTasks.bind(this);
+    this.delete = this.delete.bind(this);
+  }
+ 
+  delete(key) {
+    this.props.delete(key);
+  }
+  
+  createTasks(item) {
+    return <li onClick={(e) => this.delete(item.key, e)} 
+              key={item.key}>{item.text}</li>
+  }
+ 
+  render() {
+    var todoEntries = this.props.entries;
+    var listItems = todoEntries.map(this.createTasks);
+ 
+    return (
+      <ul className="theList">
+      <ol title="Delete this task?">
+        {listItems}
+        </ol>
+      </ul>
+    );
+  }
+};
+ 
+export default TodoItems;
